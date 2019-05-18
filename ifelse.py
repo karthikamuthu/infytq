4f844e185cd91b08ebe273f86d9eca584a9156f5
karthika Muthukumar <47214403+karthikamuthu@users.noreply.github.com>
2019-05-18 11:00:42 +0530

    ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+input My_Team_Goals
+input Opponent_Team_Goals
+if(My_Team_Goals>Opponent_Team_Goals):
+   print(“We won”)
+elif(My_Team_Goals>Opponent_Team_Goals): 
+   print(“They won”)
+else:
+   print("it is tie")
+end-if
