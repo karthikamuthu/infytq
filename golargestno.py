b673c970a1b3899779c89d87afec990e547fec88
karthika Muthukumar <47214403+karthikamuthu@users.noreply.github.com>
2019-05-18 11:12:01 +0530

    golargest.py

---
+++ golargestno.py
@@ -0,0 +1,16 @@
+
+package main
+func main(){
+    var first int=3
+    var second int=4
+    var third int=1
+    if(first>=second&&first>=third){
+    println("first is the largest")
+   }else if(second>=third){
+    println("second is the largest")
+    }else {
+    println("third is the largest")
+    }
+    
+    
+}
